077a06a7474aecd5ccf8addcb7cfc66920b7af1a
philippzalewski <kontakt@centrum-ngo.pl>
2017-03-17 23:37:19 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,10 @@ var withButtonClass = document.getElementsByClassName('button');
 // console.log(withButtonClass);
 
 var q = withButtonClass.length;
-console.log(withButtonClass.length);
+
 
 for ( var a = 0; q != a; a++) {
-
-	function textButton () {
-	console.log(withButtonClass.innerText);
-	}
+	document.getElementsByClassName('button');
+	withButtonClass.innerText = 'button';
+	alert(withButtonClass);
 }
